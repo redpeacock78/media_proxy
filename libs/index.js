78bd908676b20d4e3e3324c97f607e6b6fd8faf9
redpeacock78 <scarletpeacock78@gmail.com>
2022-12-29 21:32:27 +0900

    fix: Fix libs/index.js

--- libs/index.js
+++ libs/index.js
@@ -4,7 +4,6 @@ import figlet from "figlet";
 import helmet from "helmet";
 import express from "express";
 import { log4js } from "./log4js.js";
-import { imageFileType } from "./imageFileType.js";
 import { systemLogger, accessLogger } from "./logger.js";
 export {
   sharp,
@@ -13,7 +12,6 @@ export {
   helmet,
   figlet,
   express,
-  imageFileType,
   systemLogger,
   accessLogger,
 };
