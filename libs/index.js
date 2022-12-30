12ec4da0eac6185a8f6536df97a3cacee606a139
redpeacock78 <scarletpeacock78@gmail.com>
2022-12-30 13:21:44 +0900

    fix: Fix libs/index.js

--- libs/index.js
+++ libs/index.js
@@ -1,17 +1,9 @@
-import sharp from "sharp";
-import axios from "axios";
-import figlet from "figlet";
-import helmet from "helmet";
-import express from "express";
-import { log4js } from "./log4js.js";
-import { logger } from "./logger.js";
-
 export const libs = {
-  sharp: sharp,
-  axios: axios,
-  log4js: log4js,
-  helmet: helmet,
-  figlet: figlet,
-  express: express,
-  logger: logger,
+  sharp: (await import("sharp")).default,
+  axios: (await import("axios")).default,
+  log4js: (await import("./log4js.js")).log4js,
+  helmet: (await import("helmet")).default,
+  figlet: (await import("figlet")).default,
+  express: (await import("express")).default,
+  logger: (await import("./logger.js")).logger,
 };
