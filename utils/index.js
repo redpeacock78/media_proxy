9d231079e0be3ef4e4bb8bf79f1fa2efbefb4853
redpeacock78 <scarletpeacock78@gmail.com>
2022-12-30 11:47:22 +0900

    fix: Fix utils/index.js

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,11 @@
-export { proxyRequestHeaders } from "./proxyRequestHeaders.js";
-export { responseHeader } from "./responseHeader.js";
-export { imageFileType } from "./imageFileType.js";
-export { validURLStringDecision } from "./validURLStringDecision.js";
+import { imageFileType } from "./imageFileType.js";
+import { responseHeader } from "./responseHeader.js";
+import { proxyRequestHeaders } from "./proxyRequestHeaders.js";
+import { validURLStringDecision } from "./validURLStringDecision.js";
+
+export const utils = {
+  imageFileType: imageFileType,
+  responseHeader: responseHeader,
+  proxyRequestHeaders: proxyRequestHeaders,
+  validURLStringDecision: validURLStringDecision,
+};
