6f9f116982369771d6f1522b3ccecc4954670a02
redpeacock78 <scarletpeacock78@gmail.com>
2025-02-04 11:36:32 +0900

    add: Add functions/index.js

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+import { Client } from "appwrite";
+import { libs } from "./libs/index.js";
+import { router } from "./router/index.js";
+import { middleware } from "./middleware/index.js";
+
+const client = new Client();
+client.setProject(process.env.APPWRITE_PROJECT_ID);
+
+const app = libs.express();
+
+app.use(libs.helmet.hsts());
+app.use(libs.helmet.hidePoweredBy());
+app.use(middleware.cors);
+app.use(middleware.resourceManage);
+app.use(libs.log4js.connectLogger(libs.logger.accessLogger));
+
+app.use("/", router.root);
+app.use("/proxy", router.proxy);
+
+app.listen(process.env.PORT, () => {
+  console.info(
+    `${libs.figlet.textSync("MediaProxy", { font: "Rectangles" })}\n`
+  );
+  libs.logger.systemLogger.info("Server Start");
+});
